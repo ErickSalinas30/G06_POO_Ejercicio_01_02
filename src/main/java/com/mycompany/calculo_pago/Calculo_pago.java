f2b11e66ad21c063096c168d7ad67d5c920ef30b
usuario <usuario@DESKTOP-978SDUP>
2022-10-15 22:49:58 -0500

    mejorar

---
+++ src/main/java/com/mycompany/calculo_pago/Calculo_pago.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calculo_pago;
+
+/**
+ *
+ * @author usuario
+ */
+public class Calculo_pago {
+
+    public static void main(String[] args) {
+        
+        var empleado1=new Empleado();
+        
+        empleado1.costoHora=12.35;
+        empleado1.horasTrabajadas=152;
+        empleado1.nombre="Erick Otavalo";
+        empleado1.yearIngreso=2020;
+        
+        
+                
+        
+        
+        
+    }
+}
